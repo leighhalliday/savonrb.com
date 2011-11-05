d22e9e515d59048ce01cf854d4ed70f9a78cdfc8
rubiii <me@rubiii.com>
2011-11-05 20:05:00 +0100

    rm cookie-based animation

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,24 +1,3 @@
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
 $(function() {
 
   var url = document.location.toString();
@@ -54,21 +33,5 @@ $(function() {
 
     return false;
   });
-
-  if (document.location.toString().indexOf("#") == -1 && readCookie("savon-animate") === null) {
-    var $nav      = $("nav"),
-        $navList  = $nav.find("ol"),
-        navHeight = $nav.height();
-
-    $navList.hide();
-    $nav.css({ "height": 5, "padding-top": 0, "padding-bottom": 0 });
-
-    createCookie("savon-animate", "true", 7);
-
-    window.setTimeout(function() {
-      $nav.animate({ height: navHeight, paddingTop: 20, paddingBottom: 10 }, 1500, "easeInOutCubic");
-      $navList.show();
-    }, 1000);
-  }
 
 });
